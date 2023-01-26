b5cf6ad66bd809fa309099299e04fc8ed857419b
horrible little slime <69secret69email69@gmail.com>
2023-01-26 17:16:53 -0500

    Remove old files; add LGR; implement outfits

---
+++ src/outfit/index.ts
@@ -0,0 +1,4 @@
+export { usingThumbRing } from "./bonusgear";
+export { meatOutfit } from "./meat";
+export { freeFightOutfit } from "./free";
+export { usingPurse, refreshLatte, useUPCs, tryFillLatte } from "./lib";
